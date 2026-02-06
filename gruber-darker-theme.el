967e7d59012d3706c2ee1c14e146b2e5684d18a8
Eason0210 <aqua0210@163.com>
2026-02-07 06:33:30 +0800

    Add org-verbatim face

--- gruber-darker-theme.el
+++ gruber-darker-theme.el
@@ -340,6 +340,7 @@
    `(org-upcoming-deadline ((t (:foreground ,gruber-darker-yellow))))
    `(org-scheduled-previously ((t (:foreground ,gruber-darker-orange))))
    `(org-table ((t (:foreground ,gruber-darker-wisteria))))
+   `(org-verbatim ((t (:foreground ,gruber-darker-quartz))))
 
    ;; Search
    `(isearch ((t ,(list :foreground gruber-darker-black
